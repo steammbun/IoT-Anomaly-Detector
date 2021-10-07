4a1ac2fdabaa61431cc2580701e2ff5501447cef
steammbun <stevenkhuu1999@yahoo.com>
2021-10-07 16:54:00 -0700

    added list and navigation view

--- IoT Anomaly Detector/ContentView.swift
+++ IoT Anomaly Detector/ContentView.swift
@@ -7,24 +7,77 @@
 
 import SwiftUI
 
+struct Device: Identifiable {
+    var id = UUID()
+    var name: String
+    var icon: String
+}
+
 struct ContentView: View {
+    let devices = [
+        Device(name: "Device1", icon: "candybarphone"),
+        Device(name: "Device2", icon: "candybarphone"),
+    ]
     var body: some View {
-        NavigationView{
-            NavigationLink(destination: Text("Second View")) { Text("Hello, world!")
-            }
-                
-                .navigationBarTitle("Navigation")
-            ScrollView {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Placeholder")/*@END_MENU_TOKEN@*/
-            }
         
-            
+        NavigationView {
+            //ZStack {
+                BackgroundView()
+                //VStack {
+                    //Text("test")
+                    List(devices) { device in
+                        NavigationLink(destination: DetailView(device: device)){DeviceRow(device: device)
+                        }
+                        
+                           
+                    //}
+                    //.padding()
+                    
+                    
+                }
+                .navigationTitle("Devices")
+            //}
         }
+            
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DetailView: View {
+    var device: Device
+    
+    var body: some View {
+        VStack {
+            Text(device.name).font(.title)
+        }
+        Spacer()
+    }
+    
+}
+
+struct DeviceRow: View {
+    var device: Device
+    
+    var body: some View {
+        Label(
+            title: { Text(device.name) },
+            icon: { Image(systemName: device.icon).foregroundColor(.black) }
+            )
+        //.listRowBackground(Color.white)
+    }
+}
+struct BackgroundView: View {
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [Color.orange,Color.white
+        ]),
+        startPoint: .topLeading,
+        endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(.all)
     }
 }
