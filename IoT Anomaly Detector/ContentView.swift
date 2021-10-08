4c2e3463ea9844c8a23262fe233689cc9391171b
steammbun <stevenkhuu1999@yahoo.com>
2021-10-07 20:45:15 -0700

    sample swiftUI charts + dependencies, added some notification functionality

--- IoT Anomaly Detector/ContentView.swift
+++ IoT Anomaly Detector/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Steven Khuu on 9/30/21.
 //
 
+import UserNotifications
 import SwiftUICharts
 import SwiftUI
 
@@ -12,12 +13,13 @@ struct Device: Identifiable {
     var id = UUID()
     var name: String
     var icon: String
+    var healthy: Bool
 }
 
 struct ContentView: View {
     let devices = [
-        Device(name: "Device1", icon: "candybarphone"),
-        Device(name: "Device2", icon: "candybarphone"),
+        Device(name: "Device1", icon: "candybarphone", healthy: true),
+        Device(name: "Device2", icon: "candybarphone", healthy: false),
     ]
     var body: some View {
         
@@ -36,7 +38,13 @@ struct ContentView: View {
                     
                     
                 }
-                .navigationTitle("Devices")
+                    .navigationTitle("Devices")
+                    .onAppear(perform: {
+                        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge]) {(_,_) in
+                            
+                        }
+                    })
+                
             //}
         }
             
@@ -58,8 +66,43 @@ struct DetailView: View {
 //            Text(device.name).font(.title)
 //        }
 //      Spacer()
-        VStack { // line graph
-            LineView(data: [12,22,6,1,2,17,2], title: device.name)
+        VStack { // bar graph
+            let highIntensity = Legend(color: .orange, label: "High Intensity", order: 5)
+            let buildFitness = Legend(color: .yellow, label: "Build Fitness", order: 4)
+            let fatBurning = Legend(color: .green, label: "Fat Burning", order: 3)
+            let warmUp = Legend(color: .blue, label: "Warm Up", order: 2)
+            let low = Legend(color: .gray, label: "Low", order: 1)
+
+            let limit = DataPoint(value: 130, label: "5", legend: fatBurning)
+
+            let points: [DataPoint] = [
+                .init(value: 70, label: "1", legend: low),
+                .init(value: 90, label: "2", legend: warmUp),
+                .init(value: 91, label: "3", legend: warmUp),
+                .init(value: 92, label: "4", legend: warmUp),
+                .init(value: 130, label: "5", legend: fatBurning),
+                .init(value: 124, label: "6", legend: fatBurning),
+                .init(value: 135, label: "7", legend: fatBurning),
+                .init(value: 133, label: "8", legend: fatBurning),
+                .init(value: 136, label: "9", legend: fatBurning),
+                .init(value: 138, label: "10", legend: fatBurning),
+                .init(value: 150, label: "11", legend: buildFitness),
+                .init(value: 151, label: "12", legend: buildFitness),
+                .init(value: 150, label: "13", legend: buildFitness),
+                .init(value: 136, label: "14", legend: fatBurning),
+                .init(value: 135, label: "15", legend: fatBurning),
+                .init(value: 130, label: "16", legend: fatBurning),
+                .init(value: 130, label: "17", legend: fatBurning),
+                .init(value: 150, label: "18", legend: buildFitness),
+                .init(value: 151, label: "19", legend: buildFitness),
+                .init(value: 150, label: "20", legend: buildFitness),
+                .init(value: 160, label: "21", legend: highIntensity),
+                .init(value: 159, label: "22", legend: highIntensity),
+                .init(value: 161, label: "23", legend: highIntensity),
+                .init(value: 158, label: "24", legend: highIntensity),
+            ]
+
+            BarChartView(dataPoints: points, limit: limit)
             Spacer()
         }
         
@@ -71,11 +114,28 @@ struct DeviceRow: View {
     var device: Device
     
     var body: some View {
-        Label(
-            title: { Text(device.name) },
-            icon: { Image(systemName: device.icon).foregroundColor(.black) }
-            )
+        HStack {
+            Image(systemName: device.icon)
+            Text(device.name)
+            Spacer()
+            if(device.healthy == false) {
+            Image(systemName: "exclamationmark.triangle")
+            }
+        }
+        //Label(
+          //  title: { Text(device.name) },
+           // icon: { Image(systemName: device.icon).foregroundColor(.black) }
+            //)
         //.listRowBackground(Color.white)
+    }
+    func createNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Anomaly Detected"
+        content.subtitle = device.name
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let request = UNNotificationRequest(identifier: "IN-APP", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
     }
 }
 struct BackgroundView: View {
